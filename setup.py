01b7e80514df41b85a28d533fcf8b237deb7801d
Florian Finke <flo@randomknowledge.org>
2012-06-19 18:29:59 +0200

    added setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+import finddata
+from setuptools import setup, find_packages
+
+
+setup(
+    name="rqworker_dashboard",
+    author="Florian Finke",
+    author_email="flo@randomknowledge.org",
+    version='0.1',
+    packages=find_packages(),
+    package_data=finddata.find_package_data(
+        exclude_directories=finddata.standard_exclude_directories + ('RQWorkerDashboard',),
+        exclude=finddata.standard_exclude + 'database.db'),
+    include_package_data=True,
+)
